17bebcbde820f155ad4ddb0185aa79e6b73a9c7d
Shravan Mahesh <145256574+Shravan21105@users.noreply.github.com>
2024-10-16 02:58:54 +0530

    About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,7 +4,7 @@ import styles from "./About.module.css";
 const About = () => {
   return (
     <section className={styles.container} id = "about">
-        <h2 className={styles.title}>About</h2>
+        <h2 className={styles.title}>About Me</h2>
         <div className={styles.content}>
             <img className={styles.aboutImage}
             src={getImageUrl("about/aboutImage.png")} 
